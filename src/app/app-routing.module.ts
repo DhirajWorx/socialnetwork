a19154cb12f4dbca6e360daf8db5312ef6328d6a
DhirajWorx <34943104+DhirajWorx@users.noreply.github.com>
2019-11-10 20:27:25 +0530

    WIP Landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './landing/login/login.component';
+import { BodyComponent } from './landing/body/body.component';
+import { SignupComponent } from './landing/signup/signup.component';
+import { SearchComponent } from './landing/search/search.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: BodyComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './landing/header/header.component';
+import { LoginComponent } from './landing/login/login.component';
+import { FooterComponent } from './landing/footer/footer.component';
+import { BodyComponent } from './landing/body/body.component';
+import { HomeComponent } from './landing/home/home.component';
+import { SignupComponent } from './landing/signup/signup.component';
+import { SearchComponent } from './landing/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent,
+    BodyComponent,
+    HomeComponent,
+    SignupComponent,
+    SearchComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
